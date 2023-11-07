0f00aab958db2fce5296ebd874f144f38a2fe30b
liyatang <liyatang@qq.com>
2023-11-08 01:11:01 +0800

    feat: use vite and BrowserRouter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 // App 组件主入口
 import AppRoute from './router';
 
 const App = () => {
   return (
-    <div className="app">
-      <AppRoute />
-    </div>
+    <Router>
+      <div className="app">
+        <AppRoute />
+      </div>
+    </Router>
   );
 };
 
