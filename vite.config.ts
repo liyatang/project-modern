a64696b17de0de7071733a8dc34d9ed3cdb1e123
liyatang <liyatang@qq.com>
2023-11-09 22:25:36 +0800

    feat: vite-plugin-enhance-log

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import pages from 'vite-plugin-pages';
+import enhanceLog from 'vite-plugin-enhance-log';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pages()],
+  plugins: [react(), pages(), enhanceLog()],
 });
