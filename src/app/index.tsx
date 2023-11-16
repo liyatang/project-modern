ea933bdd6d621416245bfe16d4ed094df1c53da4
liyatang <liyatang@qq.com>
2023-11-17 00:42:54 +0800

    feat: add StrictMode

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+
 // App 组件主入口
 import AppRoute from './router';
+import { StrictMode } from 'react';
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <AppRoute />
-      </div>
-    </Router>
+    <StrictMode>
+      <Router>
+        <div className="app">
+          <AppRoute />
+        </div>
+      </Router>
+    </StrictMode>
   );
 };
 
