72e5a48eedbe9a173139430f752f90c0236b2e66
liyatang <liyatang@qq.com>
2023-11-17 00:40:25 +0800

    chore: import.meta.glob 实现动态路由

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,32 @@
-import { Suspense } from 'react';
-import { useRoutes } from 'react-router-dom';
-// @ts-ignore
-import routes from '~react-pages';
+import type { ReactNode } from 'react';
+import { Suspense, lazy } from 'react';
+import { useRoutes, Navigate } from 'react-router-dom';
 
-const AppRouter = () => {
-  return <Suspense fallback={<div>Loading...</div>}>{useRoutes(routes)}</Suspense>;
-};
+// 默认懒加载，构建时分离为独立 chunk
+const modules = import.meta.glob('../pages/**/index.tsx');
+
+const pages: { path: string; element: ReactNode }[] = [];
+
+for (const file in modules) {
+  const module: () => Promise<any> = modules[file];
+
+  const path = file.replace('../pages', '').replace('/index.tsx', '');
+  const Element = lazy(module);
+
+  pages.push({
+    path,
+    element: <Element key={path} data-path={path} />,
+  });
+}
+
+const routes = [
+  {
+    path: '/',
+    element: <Navigate to="/home" />,
+  },
+  ...pages,
+];
+
+const AppRouter = () => <Suspense fallback={<>loading</>}>{useRoutes(routes)}</Suspense>;
 
 export default AppRouter;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import pages from 'vite-plugin-pages';
 import enhanceLog from 'vite-plugin-enhance-log';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pages(), enhanceLog()],
+  plugins: [react(), enhanceLog()],
 });
